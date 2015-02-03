386a10669953ae8fa7cd20a0c472770f68a1739b
saggymac <saguyer@gmail.com>
2015-02-02 20:20:03 -0500

    Initial Commit

---
+++ inappconfig/inappconfig.h
@@ -0,0 +1,19 @@
+//
+//  inappconfig.h
+//  inappconfig
+//
+//  Created by Scott A. Guyer on 2/2/15.
+//  Copyright (c) 2015 Scott A. Guyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for inappconfig.
+FOUNDATION_EXPORT double inappconfigVersionNumber;
+
+//! Project version string for inappconfig.
+FOUNDATION_EXPORT const unsigned char inappconfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <inappconfig/PublicHeader.h>
+
+
